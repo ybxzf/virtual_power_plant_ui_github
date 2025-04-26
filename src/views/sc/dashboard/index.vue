98ccf8bd988d0e2163f000572054e6e90bfdfb46
ybxzf <517394139@qq.com>
2025-04-26 11:42:54 +0800

    大屏占位

--- src/views/sc/dashboard/index.vue
+++ src/views/sc/dashboard/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="app-container">
-大屏展示！！！！
+  <div class="dash-container">
+    <!-- 大屏展示！！！！ -->
   </div>
 </template>
+<script>
+
+export default {
+  name: 'ScDashboard',
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.dash-container {
+  background: url('../../../assets/images/bigScreen.png') no-repeat;
+  height: calc(100vh - 84px);
+  background-size: 100% 100%;
+}
+</style>
